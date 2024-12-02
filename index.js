52417c595ddf54b2e0293a7b9930beb6cdc0a3d6
Ayodo <davidayodos24@gmail.com>
2024-12-02 16:53:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+const mostProfitableNeighborhood = 'Chelsea';
+const companyCeo = 'Susan Smith'
+describe('mostProfitableNeighborhood', function () {
+  it('is declared as equal to Chelsea', function () {
+      let mostProfitableNeighborhood = 'Chelsea';
+      expect(mostProfitableNeighborhood).to.equal('Chelsea');
+  });
+});
+
+
+describe('mostProfitableNeighborhood', function () {
+    it('is declared as equal to Chelsea', function () {
+        expect(mostProfitableNeighborhood).to.equal('Chelsea');
+    });
+
+    it('is defined using let', function () {
+        expect(js).to.match(/let mostProfitableNeighborhood/, "Expected mostProfitableNeighborhood to be defined using let");
+    });
+});
+
+
+
+describe('companyCeo', function () {
+    it('is declared as equal to Susan Smith', function () {
+        expect(companyCeo).to.equal('Susan Smith');
+    });
+
+    it('is defined using let', function () {
+        expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
+    });
+});
